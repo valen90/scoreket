fa882f7073940e5a8cc6a03ea8604d32cdfcf1f2
Valen <valen@Valens-MacBook-Pro.local>
2017-03-24 16:00:28 +0100

    UserRoutes

---
+++ Sources/App/Routes/UserRoutes.swift
@@ -0,0 +1,38 @@
+import Vapor
+import HTTP
+import Routing
+
+class UserRoutes: RouteCollection {
+    typealias Wrapped = HTTP.Responder
+    func build<B: RouteBuilder>(_ builder: B) where B.Value == Wrapped {
+        let sc = builder.grouped("sc")
+        //let users = sc.grouped("users")
+
+        
+        sc.get { request in
+            return try UserController.indexView(request: request)
+        }
+        sc.post { req in
+            return try UserController.create(request: req)
+        }
+        sc.get("register") { request in
+            return try UserController.registerView(request: request)
+        }
+        sc.post("register") { request in
+            return try UserController.register(request: request)
+        }
+        sc.get("login") { request in
+            return try UserController.loginView(request: request)
+        }
+        sc.post("login") { request in
+            return try UserController.login(request: request)
+        }
+        sc.get("logout") { request in
+            return try UserController.logout(request: request)
+        }
+        sc.get("highscores") { request in
+            return try UserController.highscores(request: request)
+        }
+        
+    }
+}
